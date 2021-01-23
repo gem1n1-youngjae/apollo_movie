a4e14ef4209fc8d127dfe3dd53e237effae97e5c
유영재 <a01094741871@gmail.com>
2021-01-23 15:31:47 +0900

    [Update] router and css

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import { HashRouter as Router, Route } from 'react-router-dom'
+import Detail from '../routes/Detail'
+import Home from '../routes/Home'
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <Router>
+      <Route exact path="/" component={Home}/>
+      <Route path="/:id" component={Detail}/>
+    </Router>
   )
 }
 
